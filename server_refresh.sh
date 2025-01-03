702d4d6e57d27fb3b0d917ce7c26fc554befc02f
Skywalker290 <dev.ayusshh@gmail.com>
2025-01-03 23:42:35 +0530

    changed server to port 5003

--- server_refresh.sh
+++ server_refresh.sh
@@ -1,7 +1,7 @@
 cd ~/project02/Server02/
 git pull
 
-PID=$(lsof -t -i:5002)
+PID=$(lsof -t -i:5003)
 
 # If the PID exists, kill the process
 if [ -n "$PID" ]; then
@@ -9,7 +9,7 @@ if [ -n "$PID" ]; then
     kill -9 $PID
     echo "Gunicorn server stopped"
 else
-    echo "No Gunicorn process found on port 5002"
+    echo "No Gunicorn process found on port 5003"
 fi
 
 # Restart the Gunicorn server
@@ -18,6 +18,6 @@ echo "Starting Gunicorn server..."
 echo "============================================" >> output.log
 echo "Gunicorn server started at: $(date '+%Y-%m-%d %H:%M:%S')" >> output.log
 echo "============================================" >> output.log
-nohup gunicorn -b localhost:5002 app:app >> output.log 2>&1 &
+nohup gunicorn -b 0.0.0.0:5003 app:app >> output.log 2>&1 &
 
 echo "Gunicorn server started"
