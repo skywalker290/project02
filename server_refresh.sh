3616e9f100ecc8591c2b9933433f5df5ab457fa4
Skywalker290 <dev.ayusshh@gmail.com>
2025-01-04 00:10:55 +0530

    gunicorn path setup

--- server_refresh.sh
+++ server_refresh.sh
@@ -18,6 +18,6 @@ echo "Starting Gunicorn server..."
 echo "============================================" >> output.log
 echo "Gunicorn server started at: $(date '+%Y-%m-%d %H:%M:%S')" >> output.log
 echo "============================================" >> output.log
-nohup gunicorn -b 0.0.0.0:5003 app:app >> output.log 2>&1 &
+nohup /home/drivool/.pyenv/versions/3.10.13/envs/yolo/bin/gunicorn -b 0.0.0.0:5003 app:app >> output.log 2>&1 &
 
 echo "Gunicorn server started"
